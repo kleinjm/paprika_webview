2f0c62079534d33a30b1402b62616459b295dac4
James Klein <kleinjm007@gmail.com>
2021-12-23 17:58:00 -0500

    index page set up

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,31 @@
 class Recipe < ApplicationRecord
   self.table_name = "ZRECIPE"
 
+  alias_attribute :cooktime, :ZCOOKTIME
+  alias_attribute :created, :ZCREATED
+  alias_attribute :deleted, :ZINTRASH
+  alias_attribute :description, :ZDESCRIPTIONTEXT
+  alias_attribute :difficulty, :ZDIFFICULTY
+  alias_attribute :directions, :ZDIRECTIONS
+  alias_attribute :imageurl, :ZIMAGEURL
+  alias_attribute :ingredients, :ZINGREDIENTS
   alias_attribute :name, :ZNAME
+  alias_attribute :notes, :ZNOTES
+  alias_attribute :nutritionalinfo, :ZNUTRITIONALINFO
+  alias_attribute :photo, :ZPHOTO
+  alias_attribute :photohash, :ZPHOTOHASH
+  alias_attribute :photolarge, :ZPHOTOLARGE
+  alias_attribute :preptime, :ZPREPTIME
+  alias_attribute :rating, :ZRATING
+  alias_attribute :scale, :ZSCALE
+  alias_attribute :selecteddirection, :ZSELECTEDDIRECTION
+  alias_attribute :selectedingredients, :ZSELECTEDINGREDIENTS
+  alias_attribute :servings, :ZSERVINGS
+  alias_attribute :source, :ZSOURCE
+  alias_attribute :sourceurl, :ZSOURCEURL
+  alias_attribute :status, :ZSTATUS
+  alias_attribute :synchash, :ZSYNCHASH
+  alias_attribute :totaltime, :ZTOTALTIME
+
+  default_scope { where(deleted: false) }
 end
--- lib/tasks/data.rake
+++ lib/tasks/data.rake
@@ -4,10 +4,13 @@ namespace :data do
   desc "Sync local paprika files on a Mac to the app's database"
   task sync_paprika: :environment do
     paprika_data_dir = "$HOME/Library/Group\\\ Containers/72KVKW69K8.com.hindsightlabs.paprika.mac.v3/Data"
-    paprika_database = "#{paprika_data_dir}/Database/Paprika.sqlite"
-    paprika_photos = "#{paprika_data_dir}/Photos"
-    app_database = Rails.root.join("db/development.sqlite3")
 
+    paprika_database = "#{paprika_data_dir}/Database/Paprika.sqlite"
+    app_database = Rails.root.join("db/development.sqlite3")
     system("cp #{paprika_database} #{app_database}")
+
+    paprika_photos = "#{paprika_data_dir}/Photos"
+    app_photos = Rails.root.join("app/assets/images")
+    system("cp -r #{paprika_photos} #{app_photos}")
   end
 end
